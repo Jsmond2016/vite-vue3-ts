2de0a3a553ae519b3588f5ff63912b74280c2572
Jsmond2016 <jsmond2016@gmail.com>
2021-04-02 15:25:09 +0800

    项目初始化

---
+++ src/components/About.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <el-button type="primary" @click="goBack">回到首页</el-button>
+</template>
+<script lang="ts">
+import { defineComponent, reactive, toRefs } from 'vue'
+import { useRouter }  from 'vue-router'
+export default defineComponent({
+  name: 'About',
+  setup() {
+    const state = reactive({
+      msg: "About"
+    })
+   
+    const history = useRouter()
+    const goBack = () => {
+      history.push('/home')
+    }
+
+    return {
+      ...toRefs(state),
+      goBack,
+    }
+  },
+})
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,52 @@
+<template>
+  <h1>{{ msg }}</h1>
+
+  <p>count: {{ count }}</p>
+
+  <el-button @click="minus">减1</el-button>
+  <el-button @click="add">加1</el-button>
+  <el-button type="info" @click="jumpToAbout">跳转页面-About</el-button>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent, computed } from "vue";
+import { useStore } from "vuex";
+import { useRouter } from "vue-router"
+export default defineComponent({
+  name: "HelloWorld",
+  setup: () => {
+    const store = useStore();
+    const count = computed(() => store.state.count);
+    const add = () => {
+      store.commit("increment");
+    };
+    const minus = () => {
+      store.commit("decrement");
+    };
+    
+    const history = useRouter()
+    const jumpToAbout = () => {
+      history.push('/about')
+    }
+    return { count, add, minus, jumpToAbout };
+  },
+});
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
